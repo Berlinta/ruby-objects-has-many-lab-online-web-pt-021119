f446c85dc451dd28232a6d3e3c3afd05dcc941e6
Nash Brancic <nashfft@gmail.com>
2019-03-27 01:17:16 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,15 @@
+class Song
+  
+  attr_accessor :artist, :name
+  
+  def initialize(name)
+    @name = name
+  end
+  
+  # def add_song(song)
+  #   @songs << song
+  #   song.artist = self
+  #   @songs
+  # end
+  
+end

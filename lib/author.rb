3346385b0c4727c2adeb4d131bbeb5af6d33d4bd
Nash Brancic <nashfft@gmail.com>
2019-03-27 01:34:21 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -12,9 +12,9 @@ class Author
   end
   
   def add_post(post)
-    post.author = self
-    @posts << post
-    @@posts += 1 
+    post.author = self 
+    @posts << post 
+    @@posts << post
   end
 
 end

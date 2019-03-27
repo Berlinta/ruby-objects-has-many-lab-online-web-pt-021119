96dae938b414d323803c6b89a6d22458cb84d765
Nash Brancic <nashfft@gmail.com>
2019-03-27 01:48:25 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,11 +1,18 @@
 require 'pry'
 
 class Post
+
+  @@all = []
   
   attr_accessor :post, :author, :title
   
   def initialize(name)
     @name = name
+    @@all << self
+  end
+  
+  def self.all
+    @@all
   end
   
 end 
